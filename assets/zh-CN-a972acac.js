3d7d4c6f9a3b687f9316d325a7de4ea8197db15c
ogerly <ogerly@tutanota.com>
2025-03-14 08:07:56 +0100

    Updates

---
+++ assets/zh-CN-a972acac.js
@@ -0,0 +1,1 @@
+const r={hello:e=>{const{normalize:n,interpolate:o,named:a}=e;return n(["Hello, ",o(a("name")),"!"])},language:e=>{const{normalize:n}=e;return n(["Language"])},menu:{home:e=>{const{normalize:n}=e;return n(["Home"])}}};export{r as default};
